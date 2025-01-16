96a0c67306dd2a8f826390963b16bf45f009a2e3
MonicaLuongNZ <monica.luong.nz@gmail.com>
2025-01-16 21:24:09 +1300

    Add .gitignore and fix permission issues

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,80 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        //public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States."
+                        },
+
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan"
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "The one with the cathedral that was never really finished",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Cathedral of Our Lady",
+                            Description = "A Gothic style cathedral"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Antwerp Central Station",
+                            Description = "The finest example of railway architecture in Belgium"
+                        },
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "paris",
+                    Description = "The one with that big tower",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "The Lourve",
+                            Description = "The world's largest museum"
+                        },
+                    }
+                }
+            };
+        }
+    }
+}
