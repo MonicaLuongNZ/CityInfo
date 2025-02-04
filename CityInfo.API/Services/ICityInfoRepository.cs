f08962ae38785a3914264b623053c17c3ea29b70
MonicaLuongNZ <monica.luong.nz@gmail.com>
2025-02-05 09:48:47 +1300

    Filtering, searching, and paging

--- CityInfo.API/Services/ICityInfoRepository.cs
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -1,10 +1,13 @@
 ﻿using CityInfo.API.Entities;
+using System.Threading.Tasks;
 
 namespace CityInfo.API.Services
 {
     public interface ICityInfoRepository
     {
         Task<IEnumerable<City>> GetCitiesAsync();
+        Task<(IEnumerable<City>, PaginationMetadata)> GetCitiesAsync(
+            string? name, string? searchQuery, int pageNumber, int pageSize);
         Task<City?> GetCityAsync(int cityId, bool includePointsOfInterest);
         Task<bool> CityExistAsync(int cityId);
         Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId);
